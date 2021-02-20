8103df298ae8585c30413739eb8b0f3918e669af
MayaNurani <maya.nurani@theculturetrip.com>
2021-02-20 17:29:44 +0200

    Revert and merge "design"
    This reverts commit 269df3ad

--- main.py
+++ main.py
@@ -13,20 +13,25 @@ def raise_frame(frame_show, *frames_hide):
     frame_show.grid(padx=50,pady=50)
     frame_show.tkraise()
 
+
 # תפריט אשר עובד באמצעות הצגה והסתרה של frameים
 def setMenu(page):
     menubar = Menu(page)
-    menubar.add_command(label="Homepage", command=lambda: raise_frame(home_page, personal_details, contact,locations,summary))
-    menubar.add_command(label="Personal details", command=lambda: raise_frame(personal_details, home_page, contact,locations,summary))
-    menubar.add_command(label="Add a contact", command=lambda: raise_frame(contact, home_page, personal_details,locations,summary))
-    menubar.add_command(label="Add contact locations", command= lambda: raise_frame(locations, contact, home_page, personal_details,summary))
-    menubar.add_command(label="Contacts and locations", command= lambda: raise_frame(summary,contact,home_page,personal_details,locations))
+    menubar.add_command(label="Homepage",
+                        command=lambda: raise_frame(home_page, personal_details, contact, locations, summary))
+    menubar.add_command(label="Personal details",
+                        command=lambda: raise_frame(personal_details, home_page, contact, locations, summary))
+    menubar.add_command(label="Add a contact",
+                        command=lambda: raise_frame(contact, home_page, personal_details, locations, summary))
+    menubar.add_command(label="Add contact locations",
+                        command=lambda: raise_frame(locations, contact, home_page, personal_details, summary))
+    menubar.add_command(label="Contacts and locations",
+                        command=lambda: raise_frame(summary, contact, home_page, personal_details, locations))
     menubar.add_command(label="Exit", command=root.quit)
     root.config(menu=menubar)
 
 
 def objData(arr_keys, arr_val):
-    # insert objects to obj by using array_details - (maybe done for now)
     obj = {}
     for x, y in zip(arr_keys, arr_val):
         obj[x.cget("text")] = y.get()
@@ -35,24 +40,24 @@ def objData(arr_keys, arr_val):
     return obj
 
 
-def saveFormData(form_data, file_name,stat):
+def saveFormData(form_data, file_name, stat):
     if detailsValidation(form_data):
-        print(form_data.values())
-        print(type(form_data.values()))
-        with open("{0}.{1}".format(file_name,"txt"), "{0}".format(stat)) as file:
+        # TODO: check if needed to add try & except
+        with open("{0}.txt".format(file_name), "{0}".format(stat)) as file:
             file.write(str(form_data))
 
         # Confirmation popup - let the user know the details saved
-        sendCallBack(form_data)
-        # TODO: maybe we can close details page after the message :)
+        sendCallBack()
     else:
-        messagebox.showinfo('Validation error', 'Please finish to fill in all your details')
+        messagebox.showinfo('Validation error', 'Please finish to fill in all fields')
+
 
 def detailsValidation(form_data):
     for i in form_data.values():
         if i == '':
             return False
     return True
+
 
 def clearForm():
     for i in array_details_values:
@@ -61,9 +66,11 @@ def clearForm():
         else:
             i.delete(0, END)
 
-def sendCallBack(form_data):
-    obj_values = list(form_data.values())
-    messagebox.showinfo('confirmation', '{0}, your details saved successfully!'.format(obj_values[0]))
+
+def sendCallBack():
+    user_name = txt_full_name.get() + ','
+    messagebox.showinfo('Confirmation', '{0}\nYour details saved successfully!'.format(user_name))
+
 
 def updateContactsLocations():
     with open("users_locations.txt", "r") as locationfile:
@@ -75,21 +82,22 @@ def updateContactsLocations():
     lbl_contacts_list = Label(summary, text=contactList)
     lbl_contacts_list.grid(row=4, column=0)
 
-# Root is our main windoow (where we create frames)
+
+# Root is our main window (where we create frames)
 root = Tk()
 root.minsize(600, 250)
 root.config(bg="lightgreen")
-s= Style()
-s.configure('TFrame',foreground='#000000',background='lightgreen')
+s = Style()
+s.configure('TFrame', foreground='#000000', background='lightgreen')
 
 # Home page - the main page of our program, from this page the user can navigate between the other pages
 home_page = Frame(root)
 main_title_HP = Label(home_page,
                       text="COV-19\nEpidemiological Investigation System",
                       font=("Arial Bold", 16))
-main_title_HP.grid(row=0, column=0,padx=50,pady=50)
-lbl_startinvestigation = Label(home_page,text="to start, select an option from the menu.",font=("Arial Bold",14))
-lbl_startinvestigation.grid(row=1,column=0)
+main_title_HP.grid(row=0, column=0, padx=50, pady=50)
+lbl_startinvestigation = Label(home_page, text="to start, select an option from the menu.", font=("Arial Bold", 14))
+lbl_startinvestigation.grid(row=1, column=0)
 
 # contacts - person's contacts list
 contact = Frame(root)
@@ -109,7 +117,7 @@ txt_contact_phone = Entry(contact, width=20)
 txt_contact_phone.grid(row=3, column=1)
 btn_add_contact = Button(contact, text='Add Contact',
                          command=lambda: saveFormData(objData(array_contacts_keys, array_contacts_values),
-                                                      'contactsFile',"a"))
+                                                      'contactsFile', "a"))
 btn_add_contact.grid(row=4, column=0)
 
 # locations - person's contact locations list
@@ -117,15 +125,16 @@ locations = Frame(root)
 lbl_locations_title = Label(locations, font=("Arial Bold", 10), text="contact locations list")
 lbl_locations_title.grid(row=0, columnspan=20)
 lbl_newlocation = Label(locations, font=("Arial Bold", 10), text="Full address:")
-lbl_newlocation.grid(row=1,column=0)
+lbl_newlocation.grid(row=1, column=0)
 txt_newlocation = Entry(locations, width=20)
 txt_newlocation.grid(row=1, column=1)
 lbl_datefbeing = Label(locations, font=("Arial Bold", 10), text="Date:")
-lbl_datefbeing.grid(row=2,column=0)
+lbl_datefbeing.grid(row=2, column=0)
 txt_datefbeing = Entry(locations, width=20)
 txt_datefbeing.grid(row=2, column=1)
-btn_add_location = Button(locations, text='Add Location', command=lambda: saveFormData(objData(array_location_keys, array_location_values),
-                                               'users_locations',"a"))
+btn_add_location = Button(locations, text='Add Location',
+                          command=lambda: saveFormData(objData(array_location_keys, array_location_values),
+                                                       'users_locations', "a"))
 btn_add_location.grid(row=3, column=1)
 
 # personal details
@@ -133,19 +142,22 @@ personal_details = Frame(root)
 
 # creation
 main_title_PD = Label(personal_details,
-                      text='Personal Details:\n Please fill in the following details,according to the format',
+                      text='Personal Details:\n Please fill in the following details, according to the format',
                       font=("Arial Bold", 10))
 main_title_PD.grid(row=0, columnspan=20)
 
-btn_send = Button(personal_details, text='Send', command=lambda: saveFormData(objData(array_details_keys, array_details_values),
+btn_send = Button(personal_details, text='Send',
+                  command=lambda: saveFormData(objData(array_details_keys, array_details_values),
                                                'users_personalDetails', "w"))
 btn_clear = Button(personal_details, text='Clear', command=clearForm)
+# TODO : add clear button also to other pages?
 
 # Full Name field
 lbl_full_name = Label(personal_details, text='Full Name')
 txt_full_name = Entry(personal_details, width=20)
 
 # Age (Can be as 'date of birth' and split by Year-Month-Day)
+# TODO : check for date format
 lbl_age = Label(personal_details, text='Age')
 txt_age = Entry(personal_details, width=4)
 # numeric_age = int(txt_age.get())
@@ -162,7 +174,6 @@ cities_obj = {
 }
 lbl_city = Label(personal_details, text='City')
 combo_city = Combobox(personal_details, values=list(cities_obj.values()))
-#combo_city['values'] = (None, 'Tel-Aviv', 'Jerusalem', 'Ramat-Gan', 'Haifa', 'Yavne')
 combo_city.current(0)  # Set default value
 
 # chk_state = BooleanVar()
@@ -199,17 +210,17 @@ array_details_keys = [lbl_full_name, lbl_age, lbl_city, lbl_gender]
 array_details_values = [txt_full_name, txt_age, combo_city, var_rad]
 array_contacts_keys = [lbl_contact_name, lbl_contact_id, lbl_contact_phone]
 array_contacts_values = [txt_contact_name, txt_contact_id, txt_contact_phone]
-array_location_keys = [lbl_newlocation,lbl_datefbeing]
-array_location_values = [txt_newlocation,txt_datefbeing]
+array_location_keys = [lbl_newlocation, lbl_datefbeing]
+array_location_values = [txt_newlocation, txt_datefbeing]
 
 # summary
 summary = Frame(root)
-btn_refresh = Button(summary, text="refresh", command= lambda: updateContactsLocations())
+btn_refresh = Button(summary, text="refresh", command=lambda: updateContactsLocations())
 btn_refresh.grid(row=5, column=0)
 lbl_sumoflocations = Label(summary, text='Locations summary:')
-lbl_sumoflocations.grid(row=1,column=0)
+lbl_sumoflocations.grid(row=1, column=0)
 lbl_sumofcontacts = Label(summary, text='Contacts summary:')
-lbl_sumofcontacts.grid(row=3,column=0)
+lbl_sumofcontacts.grid(row=3, column=0)
 btn_quarantine = Button(summary, text='Send all to quarantine')
 btn_quarantine.grid(row=5,column=1)
 
